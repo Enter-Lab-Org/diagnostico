b4ce246ceb3c6c7be026a6f74d81a84dd984602e
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-11-10 11:07:27 -0600

    Refactor LoginPage to utilize FormLogin and SuccessLogin components, removing inline form and registration sections for cleaner structure

---
+++ app/login/SuccessLogin.tsx
@@ -0,0 +1,11 @@
+const SuccessLogin = () => {
+    return (
+        <div className="mx-60 flex flex-col bg-blue-50 rounded-xl align-center justify-center items-center p-14">
+            <img className="w-32" src="assets/logo.svg" alt="logo" />
+            <img src="assets/success-login.svg" alt="success-login" />
+            <p className="text-gray-400 text-xl font-extrabold text-center">Acceso correcto</p>
+        </div >
+    );
+}
+
+export default SuccessLogin;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,14 @@
 import Link from "next/link";
 import { GradientContainer } from "../components/GradientContainer";
+import { FormLogin } from "./FormLogin";
+import SuccessLogin from "./SuccessLogin";
+
+const login = false;
 
 const LoginPage = () => {
   return <GradientContainer>
     <h1 className="text-white text-2xl font-extrabold text-center">Inicia sesion o registrate para realizar tu diagnóstico</h1>
-    <div className="flex flex-col bg-blue-50 rounded-xl">
-      <div className="grid grid-cols-2">
-        {/* Formulario de login*/}
-        <div className="flex flex-col align-center justify-center items-center p-14">
-          <img className="w-32" src="assets/logo.svg" alt="logo" />
-          <form className="flex flex-col gap-4 w-full mt-10">
-            <input type="email" placeholder="Email" className="w-full p-2 rounded-md  bg-gray-300 placeholder:text-gray-500 placeholder:font-extrabold border-0" />
-            <input type="password" placeholder="Contraseña" className="w-full p-2 rounded-md  bg-gray-300 placeholder:text-gray-500 placeholder:font-extrabold border-0" />
-            <button type="submit" className="bg-blue-900 text-white p-2 rounded-xl border-blue-900 border-2 w-full font-extrabold">Iniciar sesión</button>
-            <Link href="/recuperar-contrasena" className="text-gray-500 text-sm font-extrabold text-center underline">Olvide mi contraseña</Link>
-          </form>
-        </div>
-        {/* Seccion Registro */}
-        <div className="flex flex-col bg-gray-300 rounded-xl align-center justify-center items-center p-14 gap-9">
-          <h1 className="text-blue-900 text-2xl font-extrabold text-center">Empieza tu diagnóstico</h1>
-          <p>
-            Aute ad irure sit esse reprehenderit commodo eiusmod officia enim laboris. Labore in amet ipsum mollit ut. Dolore dolor est magna veniam non minim enim exercitation anim anim fugiat enim culpa. In exercitation quis nisi aute laborum do. Aute cupidatat in proident incididunt consectetur Lorem elit aute velit incididunt velit.
-          </p>
-          <button className="text-blue-900  p-2 rounded-xl border-blue-900 border-2 w-full font-extrabold">Registrate</button>
-        </div>
-      </div>
-    </div>
+    {login ? <SuccessLogin /> : <FormLogin />}
   </GradientContainer>;
 };
 
