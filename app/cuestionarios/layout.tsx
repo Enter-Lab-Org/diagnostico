79709cabf2373d0955b2a3d9a34a55a992eff050
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-11-25 21:56:12 -0600

    Replace "Cerrar cuestionario" button with a Link component for improved navigation to the diagnostic initiation page, and adjust layout for better alignment.

--- app/cuestionarios/layout.tsx
+++ app/cuestionarios/layout.tsx
@@ -3,6 +3,7 @@
 import { APP_ROUTES } from "@/app/router/app.routes";
 import { usePathname } from "next/navigation";
 import { StatusSidebar } from "./components/StatusSidebar";
+import Link from "next/link";
 
 const cuestionariosData: Record<string, { title: string; description: string }> = {
   [APP_ROUTES.CUESTIONARIO_CULTURA_DIGITAL]: {
@@ -48,9 +49,9 @@ const CuestionarioLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="flex flex-col rounded-xl shadow-xl w-full mx-auto p-12 gap-5 text-gray-500 bg-white">
-      <div className="grid grid-cols-6 gap-4">
+      <div className="grid grid-cols-6 gap-4 items-center justify-center">
         <h1 className="cardsTitle col-span-4 col-start-2 text-2xl font-extrabold text-center text-gray-400">Ejes de diagnóstico</h1>
-        <button className="text-gray-300 text-sm border-gray-300 border-2 rounded-xl text-center">Cerrar cuestionario</button>
+        <Link href={APP_ROUTES.INICIAR_DIAGNOSTICO} className="text-gray-300 text-sm border-gray-300 border-2 rounded-xl text-center align-middle justify-center p-2">Cerrar cuestionario</Link>
       </div>
       <div className="flex flex-row gap-5">
         <StatusSidebar />
